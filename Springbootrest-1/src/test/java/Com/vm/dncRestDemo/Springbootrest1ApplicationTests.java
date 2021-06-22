704eed4aff74393fe568a8712281158e83c9af85
DELL <DELL@DESKTOP-741RIGR>
2021-06-22 11:17:38 +0530

    springbootrest

---
+++ Springbootrest-1/src/test/java/Com/vm/dncRestDemo/Springbootrest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.vm.dncRestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootrest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
